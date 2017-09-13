78d182706e3958457a72b95a48b112b2cb6bb918
Zac Johnston <zac.johnston@monash.edu>
2017-09-13 13:33:05 -0400

    [ctools] both load functions now work

--- ctools.py
+++ ctools.py
@@ -1,3 +1,4 @@
+# standard
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,9 +6,11 @@ import kepdump
 import lcdata
 import sys, os
 import emcee
+import astropy.units as u
+import astropy.constants as const
 
-# Custom
-from burstclass import *
+# homebrew
+import burstclass
 from misc import try_mkdir
 from params import *
 from printing import *
@@ -18,10 +21,10 @@ from printing import *
 #
 # Tools in progress for using X-ray burst matcher Concord
 #============================================
-
 # TODO: - function to run mcmc
 #       - save sampler for future analysis
-#       - function to plot best fit contours/lightcurves
+#       -  generalised function to plot best fit contours/lightcurves
+#============================================
 
 
 
@@ -39,7 +42,7 @@ def load_obs(source='gs1826',
                             '4u1820-303_2.681h.dat']}
 
     for ob_file in obs_files[source]:
-        b = ObservedBurst(ob_file, path=source_path)
+        b = burstclass.ObservedBurst(ob_file, path=source_path)
         obs.append(b)
 
     return obs
@@ -48,36 +51,67 @@ def load_obs(source='gs1826',
 
 def load_models(runs,
                   batches,
-                  model_path = '/home/zacpetej/projects/kepler_grids/gs1826/mean_lightcurves/',
-                  param_path = '/home/zacpetej/projects/kepler_grids/gs1826/',
                   source = 'gs1826',
                   basename = 'xrb',
-                  params_prefix = 'params_'):
+                  params_prefix = 'params',
+                  summ_prefix = 'summ',
+                  mean_path = '/home/zacpetej/projects/kepler_grids/gs1826/mean_lightcurves',
+                  source_path = '/home/zacpetej/projects/kepler_grids/gs1826/'):
     """
-    Loads model parameters and data
+    Loads a set of models (parameters and lightcurves)
     """
     #========================================================
     # Parameters
     #--------------------------------------------------------
     # runs    = [] : list of models to use
-    # batches = [] : batches that the models in runs[] belong to
+    # batches = [] : batches that the models in runs[] belong to (one-to-one correspondence)
     #========================================================
-
     models = []
 
     for i, run in enumerate(runs):
         batch = batches[i]
-        param_str = '{prefix}{source}_{batch}.txt'.format(prefix=params_prefix,
-                        source=source, batch=batch)
-        model_str = '{source}_{batch}_{base}{run}_mean.data'.format(source=source,
-                        batch=batch, base=basename, run=run)
-        param_file = os.path.join(param_path, param_str)
 
-        # TODO: -load model params
-        #       -load recurrence times from summ
-        # ===== Load model burst data =====
-        ptable = pd.read_table(param_file, delim_whitespace=True)
-        # m = KeplerBurst()
-        # models.append(m)
+        batch_str = '{source}_{batch}'.format(source=source, batch=batch)
+        mean_str = '{batch_str}_{base}{run}_mean.data'.format(batch_str=batch_str, base=basename, run=run)
+        param_str = '{prefix}_{batch_str}.txt'.format(prefix=params_prefix, batch_str=batch_str)
+        summ_str = '{prefix}_{batch_str}.txt'.format(prefix=summ_prefix, batch_str=batch_str)
+
+        param_file = os.path.join(source_path, param_str)
+        mean_file = os.path.join(mean_path, mean_str)   # currently not used
+        summ_file = os.path.join(source_path, summ_str)
+
+        #----------------------------------
+        # TODO: - account for different Eddington composition
+        #----------------------------------
+
+        mtable = pd.read_csv(summ_file)
+        ptable = pd.read_table(param_file, delim_whitespace=True)  # parameter table
+        idx = np.where(ptable['id'] == run)[0][0]    # index of model/run
+        # NOTE: Assumes that models in ptable exactly match those in mtable
+
+        # ====== Extract model parameters/properties ======
+        xi = 1.12             # currently constant, could change in future
+        R_NS = 12.1 * u.km    # add this as colum to parameter file (or g)?
+        M_NS = ptable['mass'][idx] * const.M_sun
+        X = ptable['x'][idx]
+        Z = ptable['z'][idx]
+        lAcc = ptable['accrate'][idx] * ptable['xi'][idx]    # includes xi_p multiplier
+        opz = 1./sqrt(1.-2.*const.G*M_NS/(const.c**2*R_NS))
+        g = const.G*M_NS/(R_NS**2/opz)
+        tdel = mtable['tDel'][idx]
+        tdel_err = mtable['uTDel'][idx]
+
+        m = burstclass.KeplerBurst(filename = mean_str,
+                        path = mean_path,
+                        tdel = tdel,
+                        tdel_err = tdel_err,
+                        g = g,
+                        R_NS = R_NS,
+                        xi = xi,
+                        lAcc = lAcc,
+                        Z = Z,
+                        X = X)
+
+        models.append(m)
 
     return models
