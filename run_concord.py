da798677f7c82e5fd6fecaf00813277dd19c83ae
Zac Johnston <zac.johnston@monash.edu>
2017-11-14 16:25:14 -0500

    no longer save lnprob and rstate files

--- run_concord.py
+++ run_concord.py
@@ -24,18 +24,12 @@ print('Loading observed and model data: Run {r} from batches [{b1}, {b2}, {b3}]'
 #print('Loading observed and model data: Run {r} from batches [{b1}]'.format(r=run, b1=batches[0]))  #!!!
 
 obs = ctools.load_obs(source)
-#obs = ctools.load_obs(source)[1] # !!!
-
-
 models = ctools.load_models(runs=[run,run,run], batches=batches)
-#models = ctools.load_models(runs=[run], source=source, batches=batches)[0] #!!!
 
 pos = ctools.setup_positions(obs)
 sampler = ctools.setup_sampler(obs=obs, models=models, nwalkers=200, threads=threads)
 
 batch_str = '{src}_{b1}-{b2}-{b3}_R{r}'.format(src=source, b1=batches[0], b2=batches[1], b3=batches[2], r=run)
-#batch_str = '{src}_B{b1}_R{r}'.format(src=source, b1=batches[0], r=run)  #!!!
-
 chain_path = os.path.join(GRIDS_PATH, source, 'concord')
 
 # TODO: restarting needs testing/debugging
@@ -68,13 +62,13 @@ for i in range(n0, n0+iters):
     step_str = '{batch}_S{step}_C{cv:02}'.format(batch=batch_str, step=step+nsteps, cv=con_ver)
 
     chain_filepath = os.path.join(chain_path, 'chain_' + step_str)
-    lnprob_filepath = os.path.join(chain_path, 'lnprob_' + step_str)
-    rstate_filepath = os.path.join(chain_path, 'rstate_' + step_str + '.pkl')
+    # lnprob_filepath = os.path.join(chain_path, 'lnprob_' + step_str)
+    # rstate_filepath = os.path.join(chain_path, 'rstate_' + step_str + '.pkl')
 
     np.save(chain_filepath, sampler.chain)
-    np.save(lnprob_filepath, np.array(lnprob))
+    # np.save(lnprob_filepath, np.array(lnprob))
 
-    with open(rstate_filepath, 'wb') as output:
-        pickle.dump(rstate, output, -1)
+    # with open(rstate_filepath, 'wb') as output:
+    #     pickle.dump(rstate, output, -1)
 
 print('Done!')
