92f7cadc0adb2d3742b876a60a0fe709a261ef70
Zac Johnston <zac.johnston@monash.edu>
2017-11-20 21:56:10 -0500

    [con_versions] split off definitions of con_ver parameters to own module

---
+++ con_versions.py
@@ -0,0 +1,28 @@
+#===============================================================
+# Tools that return parameters for
+# different concord versions (con_ver)
+#===============================================================
+
+def get_weights(con_ver):
+    """========================================================
+    Returns pre-defined lhood weights, based on con_ver
+    ========================================================"""
+    tdelwts = {1:1., 2:2.5e3, 3:100., 4:2.5e3, 5:2.5e3}   # tdel weights for fitting (for different con_ver)
+    fluxwts = {1:1., 2:1., 3:1., 4:1., 5:100.}
+    return {'tdelwt':tdelwts[con_ver], 'fluxwt':fluxwts[con_ver]}
+
+
+def get_disc_model(con_ver):
+    """========================================================
+    Returns pre-defined disc model of given con_ver
+    ========================================================"""
+    discs = {1:'he16_a', 2:'he16_a', 3:'he16_a', 4:'he16_d', 5:'he16_d'}   # tdel weights for fitting (for different con_ver)
+    return discs[con_ver]
+
+
+def get_lhood_floor(con_ver):
+    """========================================================
+    Returns lhood floor for plotting
+    ========================================================"""
+    floors = {2:-5e4, 3:-1e4, 4:-1e4, 5:-1e4}    # lower cutoff for lhood plotting
+    return floors[con_ver]
